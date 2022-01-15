88b30af7a0124e21e403e00bc0a9c780f54fe949
nichojohnson84 <nickcjohnson84@outlook.com>
2022-01-15 08:17:54 -0700

    firt draft

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+"hello World"
+console.log
+var countDownDate =60000
+var x = setInterval(function() {
+    var distance = 60000 - 0;
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+});
+
+const questions1 = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo").innerHTML = questions1;  
+
+function myFunction() {
+  points.sort(function(a, b){return 0.5 - Math.random()});
+  document.getElementById("demo").innerHTML = questions1;
+}
